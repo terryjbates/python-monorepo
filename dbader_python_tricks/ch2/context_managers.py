2d0929fcb5e8568a94824d9ec05856b08410c402
Terry Bates <tbates@linkedin.com>
2018-06-01 18:35:26 -0700

    Complete context_managers examples

---
+++ dbader_python_tricks/ch2/context_managers.py
@@ -0,0 +1,39 @@
+from contextlib import contextmanager
+
+
+class ManagedFile:
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        self.file = open(self.name, 'w')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            self.file.close()
+
+
+# Pretty API example
+class Indenter:
+    def __init__(self):
+        self.level = 0
+
+    def __enter__(self):
+        self.level += 1
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.level -= 1
+
+    def print(self, text):
+        print('    ' * self.level + text)
+
+
+@contextmanager
+def managed_file(name):
+    try:
+        f = open(name, 'w')
+        yield f
+    finally:
+        f.close()
